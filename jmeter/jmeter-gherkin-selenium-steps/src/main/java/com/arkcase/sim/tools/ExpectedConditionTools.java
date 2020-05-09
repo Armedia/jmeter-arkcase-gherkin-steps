ebe1320f514995384b843b9c68413392c5fe3ce2
Diego Rivera <diego.rivera@armedia.com>
2020-05-09 09:06:15 -0600

    Bugfix: make sure we ignore "ignorable" exceptions from Selenium

--- jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/tools/ExpectedConditionTools.java
+++ jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/tools/ExpectedConditionTools.java
@@ -4,22 +4,22 @@
  * %%
  * Copyright (C) 2020 Armedia, LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -31,6 +31,8 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -41,8 +43,12 @@ public class ExpectedConditionTools {
 	public static ExpectedCondition<WebElement> firstOf(ExpectedCondition<WebElement>... locators) {
 		return (d) -> {
 			for (ExpectedCondition<WebElement> c : locators) {
-				WebElement e = c.apply(d);
-				if (e != null) { return e; }
+				try {
+					WebElement e = c.apply(d);
+					if (e != null) { return e; }
+				} catch (NoSuchElementException | StaleElementReferenceException e) {
+					// ignore...try the next one...
+				}
 			}
 			return null;
 		};
