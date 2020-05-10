499fa34ed92e722e712827215f6c188b6d4fa93a
Diego Rivera <diego.rivera@armedia.com>
2020-05-10 14:37:37 -0600

    Added code to allow waiting for more than one page on PDFTron

--- jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/PDFTronSteps.java
+++ jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/PDFTronSteps.java
@@ -26,14 +26,19 @@
  */
 package com.arkcase.sim.gherkin.steps.components;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.jbehave.core.annotations.Alias;
 import org.jbehave.core.annotations.Aliases;
 import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.arkcase.sim.components.WebDriverHelper.WaitType;
 import com.arkcase.sim.components.html.WaitHelper;
@@ -43,6 +48,7 @@ import com.arkcase.sim.tools.ByTools;
 public class PDFTronSteps extends BasicWebDriverSteps {
 
 	private static final By IFRAME = By.cssSelector("ark-document-viewer iframe");
+	private static final By PAGE_COUNTER = By.cssSelector("div#totalPages");
 	private static final By PAGE_CONTAINER = By.cssSelector("div.pageContainer");
 	private static final By PAGE_THUMBNAIL = By.cssSelector("div.thumbContainer");
 
@@ -58,23 +64,58 @@ public class PDFTronSteps extends BasicWebDriverSteps {
 		// Switch to the iframe
 		WebDriver browser = getBrowser();
 		browser.switchTo().frame(iframe);
-		WebElement docViewer = wh.waitForElement(
-			ByTools.byOneEach(PDFTronSteps.PAGE_CONTAINER, PDFTronSteps.PAGE_THUMBNAIL), WaitType.PRESENT);
-		if (!docViewer.isDisplayed()) { throw new RuntimeException("PDFTron is not ready"); }
-		browser.switchTo().parentFrame();
+		try {
+			WebElement docViewer = wh.waitForElement(
+				ByTools.byOneEach(PDFTronSteps.PAGE_CONTAINER, PDFTronSteps.PAGE_THUMBNAIL), WaitType.PRESENT);
+			if (!docViewer.isDisplayed()) { throw new RuntimeException("PDFTron is not ready"); }
+		} finally {
+			browser.switchTo().parentFrame();
+		}
 	}
 
-	@When("waiting for PDFTron")
-	@Alias("waiting for pdftron")
-	public void waitForPDFTron() {
+	@When("waiting for PDFTron to load $pages pages")
+	@Alias("waiting for pdftron to load $pages pages")
+	public void waitForPDFTron(@Named("pages") int pages) {
 		WaitHelper wh = getWaitHelper();
 		WebElement iframe = wh.waitForElement(PDFTronSteps.IFRAME, WaitType.VISIBLE);
 		// Switch to the iframe
 		WebDriver browser = getBrowser();
 		browser.switchTo().frame(iframe);
-		wh.waitForElement(ByTools.byOneEach(PDFTronSteps.PAGE_CONTAINER, PDFTronSteps.PAGE_THUMBNAIL),
-			WaitType.VISIBLE);
-		browser.switchTo().parentFrame();
+		try {
+			wh.waitForElement(ByTools.byOneEach(PDFTronSteps.PAGE_CONTAINER, PDFTronSteps.PAGE_THUMBNAIL),
+				WaitType.VISIBLE);
+			if (pages > 1) {
+				WebElement counter = wh.waitForElement(PDFTronSteps.PAGE_COUNTER, WaitType.VISIBLE);
+				String text = StringUtils.strip(counter.getText().substring(1));
+				final int totalPages = Integer.valueOf(text);
+				if (pages > totalPages) {
+					pages = totalPages;
+				}
+				while (true) {
+					List<WebElement> thumbnails = wh
+						.waitUntil(ExpectedConditions.presenceOfAllElementsLocatedBy(PDFTronSteps.PAGE_THUMBNAIL));
+					if (thumbnails.size() >= pages) {
+						break;
+					}
+				}
+				"".hashCode();
+			}
+		} finally {
+			// Important cleanup
+			browser.switchTo().parentFrame();
+		}
+	}
+
+	@Then("wait for PDFTron to load $pages pages")
+	@Alias("wait for pdftron to load $pages pages")
+	public void waitForPDFTron2(@Named("pages") int pages) {
+		waitForPDFTron(pages);
+	}
+
+	@When("waiting for PDFTron")
+	@Alias("waiting for pdftron")
+	public void waitForPDFTron() {
+		waitForPDFTron(1);
 	}
 
 	@Then("wait for PDFTron")
