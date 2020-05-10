fc8a9a1fc372caf439f908519a83b59a0231b523
Diego Rivera <diego.rivera@armedia.com>
2020-05-09 19:17:16 -0600

    Finished the first implementation draft

--- jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/CreateFormSteps.java
+++ jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/CreateFormSteps.java
@@ -27,16 +27,17 @@
 package com.arkcase.sim.gherkin.steps.components;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.jbehave.core.annotations.Alias;
+import org.jbehave.core.annotations.Aliases;
 import org.jbehave.core.annotations.BeforeStory;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.jbehave.core.model.ExamplesTable;
-import org.openqa.selenium.WebElement;
 
 public class CreateFormSteps extends AbstractFormData {
 
@@ -60,6 +61,34 @@ public class CreateFormSteps extends AbstractFormData {
 		super(CreateFormSteps.TABS);
 	}
 
+	private Live.Tab tab() {
+		return tab(null);
+	}
+
+	private Live.Tab tab(String name) {
+		if (name != null) {
+			Live.Tab tab = getTab(name);
+			if (tab == null) { throw new RuntimeException("No tab named [" + name + "] was found"); }
+			this.currentTab = tab;
+		}
+		if (this.currentTab == null) { throw new RuntimeException("No tab is currently selected for work!"); }
+		return this.currentTab;
+	}
+
+	private Live.Section section() {
+		return section(null);
+	}
+
+	private Live.Section section(String name) {
+		if (name != null) {
+			Live.Section section = tab().getSection(name);
+			if (section == null) { throw new RuntimeException("No section named [" + name + "] was found"); }
+			this.currentSection = section;
+		}
+		if (this.currentSection == null) { throw new RuntimeException("No section is currently selected for work!"); }
+		return this.currentSection;
+	}
+
 	@BeforeStory
 	protected void resetState() {
 		this.currentTab = null;
@@ -67,30 +96,244 @@ public class CreateFormSteps extends AbstractFormData {
 	}
 
 	@Given("the $tab tab is active")
+	@Alias("the $tab tab is selected")
 	public void checkTabIsActive(@Named("tab") String tab) {
-		// TODO:
+		if (!tab(tab).isSelected()) { throw new RuntimeException("The [" + tab + "] tab is not active"); }
 	}
 
 	@When("activating the $tab tab")
-	@Then("activate the $tab tab")
+	@Alias("selecting the $tab tab")
 	public void activateTab(@Named("tab") String tab) {
-		// TODO:
+		tab(tab).select();
 	}
 
-	private void setFieldValue(String sectionName, WebElement section, String label, String value) {
-		// What fieldType of field is it? Set the value accordingly
-		// TODO: Convert the value to the field's required format, and set it
-		// TODO: Explode if the value is not convertable
+	@Then("activate the $tab tab")
+	@Alias("select the $tab tab")
+	public void activateTab2(@Named("tab") String tab) {
+		activateTab(tab);
+	}
+
+	@Given("the $section section is expanded")
+	@Aliases(values = {
+		"the $section section is visible", //
+		"the $section section is shown", //
+	})
+	public void checkSectionIsExpanded(@Named("section") String section) {
+		if (!section(section).isExpanded()) {
+			throw new RuntimeException("The [" + section + "] section is not expanded");
+		}
+	}
+
+	@Given("the section is expanded")
+	@Aliases(values = {
+		"the section is visible", //
+		"the section is shown", //
+	})
+	public void checkSectionIsExpanded() {
+		checkSectionIsExpanded(null);
+	}
+
+	@Given("the $section section is collapsed")
+	@Aliases(values = {
+		"the $section section is invisible", //
+		"the $section section is hidden", //
+	})
+	public void checkSectionIsCollapsed(@Named("section") String section) {
+		if (section(section).isExpanded()) { throw new RuntimeException("The section [" + section + "] is expanded"); }
+	}
+
+	@Given("the section is collapsed")
+	@Aliases(values = {
+		"the section is invisible", //
+		"the section is hidden", //
+	})
+	public void checkSectionIsCollapsed() {
+		checkSectionIsCollapsed(null);
+	}
+
+	@When("expanding the $section section")
+	@Aliases(values = {
+		"activating the $section section", //
+		"showing the $section section", //
+	})
+	public void expandSection(@Named("section") String section) {
+		section(section).expand();
+	}
+
+	@Then("expand the $section section")
+	@Aliases(values = {
+		"activate the $section section", //
+		"show the $section section", //
+	})
+	public void expandSection2(@Named("section") String section) {
+		expandSection(section);
+	}
+
+	@Then("expand the section")
+	@Aliases(values = {
+		"activate the section", //
+		"show the section", //
+	})
+	public void expandSection3() {
+		section().expand();
+	}
+
+	@When("collapsing the $section section")
+	@Alias("hiding the $section section")
+	public void collapseSection(@Named("section") String section) {
+		section(section).collapse();
+	}
+
+	@Then("collapse the $section section")
+	@Alias("hide the $section section")
+	public void collapseSection2(@Named("section") String section) {
+		collapseSection(section);
+	}
+
+	@Then("collapse the section")
+	@Alias("hide the section")
+	public void collapseSection3() {
+		section().collapse();
+	}
+
+	@When("toggling the $section section")
+	@Then("toggle the $section section")
+	public void toggleSection(@Named("section") String section) {
+		section(section).toggle();
+	}
+
+	@When("toggling the section")
+	@Then("toggle the section")
+	public void toggleSection() {
+		section().toggle();
+	}
+
+	private void setFieldValue(Live.Section section, String name, String value) {
+		Live.Field field = section.getField(name);
+		if (field == null) {
+			throw new RuntimeException("No field named [" + name + "] in section [" + section.getName() + "] from tab ["
+				+ section.getTab().getName() + "]");
+		}
+		field.setValue(value);
+	}
+
+	private void setFieldValues(Live.Section section, ExamplesTable values) {
+		final int rows = values.getRowCount();
+		for (int i = 0; i < rows; i++) {
+			Map<String, String> row = values.getRow(rows);
+			String field = row.get("name");
+			if (field == null) {
+				// No field name... warn and skip? Or explode?
+				throw new RuntimeException("No field name given for row # " + i + " = " + row);
+			}
+			String value = row.get("value");
+			setFieldValue(section, field, value);
+		}
+	}
+
+	@When("populating the section with: $values")
+	@Aliases(values = {
+		"filling the section with: $values", //
+		"filling in the section with: $values", //
+	})
+	public void fillInFields(@Named("values") ExamplesTable values) {
+		setFieldValues(section(), values);
+	}
+
+	@Then("populate the section with: $values")
+	@Aliases(values = {
+		"fill the section with: $values", //
+		"fill in the section with: $values", //
+	})
+	public void fillInFields2(@Named("values") ExamplesTable values) {
+		fillInFields(values);
+	}
+
+	@When("setting the field $field to $value")
+	@Alias("setting the field $field to [$value]")
+	public void fillInField(@Named("field") String field, @Named("value") String value) {
+		setFieldValue(section(), field, value);
+	}
+
+	@Then("set the field $field to $value")
+	@Alias("set the field $field to [$value]")
+	public void fillInField2(@Named("field") String field, @Named("value") String value) {
+		fillInField(field, value);
+	}
+
+	@When("populating the $section section with: $values")
+	@Aliases(values = {
+		"filling the $section section with: $values", //
+		"filling in the $section section with: $values", //
+	})
+	public void fillInFields(@Named("section") String section, @Named("values") ExamplesTable values) {
+		setFieldValues(section(section), values);
 	}
 
 	@Then("populate the $section section with: $values")
-	public void fillInFields(@Named("section") String section, @Named("values") ExamplesTable values) {
+	@Aliases(values = {
+		"fill the $section section with: $values", //
+		"fill in the $section section with: $values", //
+	})
+	public void fillInFields2(@Named("section") String section, @Named("values") ExamplesTable values) {
+		fillInFields(section, values);
+	}
+
+	@When("setting the $section field $field to $value")
+	@Alias("setting the $section field $field to [$value]")
+	public void fillInField(@Named("section") String section, @Named("field") String field,
+		@Named("value") String value) {
+		setFieldValue(section(section), field, value);
 	}
 
 	@Then("set the $section field $field to $value")
 	@Alias("set the $section field $field to [$value]")
-	public void fillInField(@Named("section") String section, @Named("field") String field,
+	public void fillInField2(@Named("section") String section, @Named("field") String field,
 		@Named("value") String value) {
-		setFieldValue(section, null, field, value);
+		fillInField(section, field, value);
+	}
+
+	@When("clearing the $section field $field")
+	@Then("clear the $section field $field")
+	public void clearField(@Named("section") String section, @Named("field") String field) {
+		setFieldValue(section(section), field, null);
+	}
+
+	@When("clearing the field $field")
+	@Then("clear the field $field")
+	public void clearField(@Named("field") String field) {
+		clearField(null, field);
+	}
+
+	@When("clearing the $section fields: $fields")
+	@Then("clear the $section fields: $fields")
+	public void clearFields(@Named("section") String section, @Named("fields") List<String> fields) {
+		Live.Section s = section(section);
+		if ((fields != null) && !fields.isEmpty()) {
+			for (String f : fields) {
+				setFieldValue(s, f, null);
+			}
+		}
+	}
+
+	@When("clearing the fields: $fields")
+	@Then("clear the fields: $fields")
+	public void clearFields(@Named("fields") List<String> fields) {
+		clearFields(null, fields);
+	}
+
+	@When("clearing all the fields in section $section")
+	@Then("clear all the fields in section $section")
+	public void clearAllFields(@Named("section") String section) {
+		Live.Section s = section(section);
+		for (String f : s.getFieldNames()) {
+			setFieldValue(s, f, null);
+		}
+	}
+
+	@When("clearing all the fields")
+	@Then("clear all the fields")
+	public void clearAllFields() {
+		clearAllFields(null);
 	}
 }
