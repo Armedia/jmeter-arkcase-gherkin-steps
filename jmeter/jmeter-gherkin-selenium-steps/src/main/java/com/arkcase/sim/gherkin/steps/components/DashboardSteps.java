215befe8d6df809e6bac81df281f2a6a174ea9fb
Diego Rivera <diego.rivera@armedia.com>
2020-05-09 20:30:16 -0600

    BUGFIX: Collision on action names

--- jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/DashboardSteps.java
+++ jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/DashboardSteps.java
@@ -4,22 +4,22 @@
  * %%
  * Copyright (C) 2020 Armedia, LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -114,8 +114,8 @@ public class DashboardSteps extends ComponentSteps {
 		wh.waitForElement(DashboardSteps.LOGOUT_LINK, WaitType.CLICKABLE).click();
 	}
 
-	@When("selecting the $area tab")
-	@Then("select the $area tab")
+	@When("selecting the $area area")
+	@Then("select the $area area")
 	public void selectTab(@Named("area") String area) {
 		getAreaSelector(area).click();
 	}
