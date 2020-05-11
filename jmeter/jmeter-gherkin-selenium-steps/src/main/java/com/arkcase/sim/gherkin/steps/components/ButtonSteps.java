e2020a567f8c563fa99397c39a87985a3f89347b
Diego Rivera <diego.rivera@armedia.com>
2020-05-11 15:33:10 -0600

    Added more buttons and came up with a backup mechanism to find buttons

--- jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/ButtonSteps.java
+++ jmeter/jmeter-gherkin-selenium-steps/src/main/java/com/arkcase/sim/gherkin/steps/components/ButtonSteps.java
@@ -4,22 +4,22 @@
  * %%
  * Copyright (C) 2020 Armedia, LLC
  * %%
- * This file is part of the ArkCase software. 
- * 
- * If the software was purchased under a paid ArkCase license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * ArkCase is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * ArkCase is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -29,6 +29,7 @@ package com.arkcase.sim.gherkin.steps.components;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -55,6 +56,7 @@ public class ButtonSteps extends ComponentSteps {
 		final By moduleHeader = By.cssSelector("div.module-header");
 		final By footerPanel = By.cssSelector("footer.panel");
 		final By topButtonGroup = new ByChained(By.cssSelector("div.content-body"), By.cssSelector("div.btn-group"));
+		final By createOrderForm = By.cssSelector("ng-form[name=\"createNewOrderForm\"]");
 		Object[][] buttons = {
 			{
 				By.cssSelector("div.idp img[alt=\"Sharecare\"]"), //
@@ -120,6 +122,11 @@ public class ButtonSteps extends ComponentSteps {
 			}, {
 				new ByChained(topButtonGroup, By.cssSelector("button.btn-primary[permission=\"printOrder\"]")), //
 				"Central Print"
+			}, {
+				new ByChained(createOrderForm,
+					By.cssSelector("ng-form[name=\"shippingBillingForm\"] panel-view[header=\"Requester Specifics\"]"),
+					By.xpath(".//button[normalize-space(.) = \"Search Existing Requesters\"]")), //
+				"Search Existing Requesters"
 			},
 			// TODO: Add MORE buttons so it's easier to write the code
 		};
@@ -157,9 +164,18 @@ public class ButtonSteps extends ComponentSteps {
 		try {
 			return browser.findElement(by);
 		} catch (final NoSuchElementException e) {
+			WebElement element = getButtonByLabel(name);
+			if (element != null) { return element; }
 			if (!required) { return null; }
 			throw e;
 		}
+	}
+
+	private WebElement getButtonByLabel(String label) {
+		WebDriver browser = getBrowser();
+		List<WebElement> elements = browser.findElements(By.xpath("//button[normalize-space(.) = \"" + label + "\"]"));
+		if ((elements == null) || elements.isEmpty()) { return null; }
+		return elements.get(0);
 	}
 
 	@Given("the $name button is visible")
